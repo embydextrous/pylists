b9840d963795e49fd82692e5200e0e3e1e1a084b
embydextrous <arjitagarwal099@gmail.com>
2020-08-25 20:46:34 +0530

    Find nth node from end

--- getNth.py
+++ getNth.py
@@ -10,6 +10,19 @@ def getNth(node, n):
     else:
         return -1
 
+def getNthFromEnd(node, n):
+    a, b = node, node
+    for i in range(n):
+        if a:
+            a = a.next
+        else:
+            return -1
+    if a is None:
+        return -1
+    while a.next:
+        a, b = a.next, b.next
+    return b.data
+
 a = LinkedList()
 for i in range(1, 8):
     a.append(i)
@@ -17,3 +30,6 @@ a.printList()
 
 for i in range(0, 9):
     print getNth(a.head, i)
+
+for i in range(0, 9):
+    print getNthFromEnd(a.head, i)
