0c856140dd350873982333501e43c13f781e3c20
embydextrous <arjitagarwal099@gmail.com>
2020-09-22 21:29:05 +0530

    Merge alternate method 2

--- mergeAtAlternatePositions.py
+++ mergeAtAlternatePositions.py
@@ -13,21 +13,36 @@ def merge(a, b):
         tail.next = aNode
         tail = tail.next
         aNode = aNode.next
-    if aNode:
-        tail.next = aNode
     if bNode:
         tail.next = bNode
-    b.head = None
+        bNode = bNode.next
+        tail.next.next = None
+    b.head = bNode
     a.head = head
 
+def merge2(a, b):
+    if a.head is None or b.head is None:
+        return
+    aNode = a.head
+    bNode = b.head
+    while aNode and bNode:
+        next = bNode.next
+        bNode.next = aNode.next
+        aNode.next = bNode
+        b.head = next
+        aNode = bNode.next
+        bNode = next
+
+
+
 a = LinkedList()
-for i in range(6):
+for i in range(3):
     a.append(i)
 a.printList()
 b = LinkedList()
 for i in range(6, 12):
     b.append(i)
 b.printList()
-merge(a, b)
+merge2(a, b)
 a.printList()
 b.printList()
