5bb07ce2f4fbfc3c98dc5e729512d933a8b2b092
embydextrous <arjitagarwal099@gmail.com>
2020-09-24 18:56:25 +0530

    Check if a singly linked list is circular

---
+++ circularlists/checkIfSinglyLinkedListIsCircular.py
@@ -0,0 +1,39 @@
+from list import LinkedList
+from list import Node
+
+def isCircular(head):
+    if head is None:
+        return True
+    current = head
+    loopNode = findLoopNode(head)
+    if loopNode:
+        while current:
+            if current.next == head:
+                return True
+            if current.next == loopNode:
+                return False
+            current = current.next
+    else:
+        while current:
+            if current.next == head:
+                return True
+            current = current.next
+    return False
+
+def findLoopNode(head):
+    fast, slow = head, head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            return slow
+    return None
+
+a = LinkedList()
+a.append(1)
+a.append(2)
+a.append(3)
+a.append(4)
+a.append(5)
+a.head.next.next.next.next.next = a.head.next
+print isCircular(a.head)
---
+++ circularlists/list.py
@@ -0,0 +1,90 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __lt__(self, other):
+        return self.data < other.data
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # insertion at head
+    def push(self, data):
+        newNode = Node(data)
+        newNode.next = self.head
+        self.head = newNode
+
+    def append(self, data):
+        if self.head is None:
+            self.push(data)
+        else:
+            newNode = Node(data)
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = newNode
+
+    def insertAfter(self, data, key):
+        if self.head is None:
+            return
+        current = self.head
+        while current and current.data != key:
+            current = current.next
+        if current:
+            newNode = Node(data)
+            newNode.next = current.next
+            current.next = newNode
+
+    def deleteKey(self, key):
+        if self.head is None:
+            return
+        current, prev = self.head, None
+        while current and current.data != key:
+            current, prev = current.next, current
+        if current is None:
+            return
+        if prev:
+            prev.next = current.next
+        else:
+            self.head = current.next
+        current = None
+
+    # position is 0-indexed
+    def deleteAt(self, position):
+        if position < 0:
+            return
+        current, prev = self.head, None
+        while current and position != 0:
+            current, prev = current.next, current
+            position -= 1
+        if current is None:
+            return    
+        if prev:
+            prev.next = current.next
+        else:
+            self.head = current.next
+        current = None
+
+    def delete(self):
+        while self.head:
+            current = self.head
+            self.head = current.next
+            current = None
+
+    def size(self):
+        n = 0
+        current = self.head
+        while current:
+            current = current.next
+            n += 1
+        return n
+
+    def printList(self):
+        print "head ->",
+        current = self.head
+        while current:
+            print str(current.data) + " ->",
+            current = current.next
+        print "null"
