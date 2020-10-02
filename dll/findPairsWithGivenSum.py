f963e691d4e352da9f6da9ebaac3b4a12c8f32df
embydextrous <arjitagarwal099@gmail.com>
2020-10-02 18:20:04 +0530

    Find pairs with given sum

---
+++ dll/findPairsWithGivenSum.py
@@ -0,0 +1,23 @@
+from dll import DLL
+from dll import Node
+
+def findPairsWithSumX(head, tail, x):
+    result = []
+    if head is None or tail is None:
+        return result
+    while head != tail and tail.next != head:
+        if head.data + tail.data == x:
+            result.append((head.data, tail.data))
+            head, tail = head.next, tail.prev
+        elif head.data + tail.data < x:
+            head = head.next
+        else:
+            tail = tail.prev
+    return result
+
+s = [1]
+dll = DLL()
+for i in s:
+    dll.append(i)
+dll.printList()
+print findPairsWithSumX(dll.head, dll.tail, 7)
